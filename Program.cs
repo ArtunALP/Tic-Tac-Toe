2cace1899fa7fd47d2296124624614314dfb921d
ArtunALP <artunalp1@gmail.com>
2021-04-23 04:40:10 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ForegroundColor = ConsoleColor.White;
+           Console.WindowHeight = 40;
+           
+        }
+    }
+}
