a73e91103e4c8bac20ede603b6f5bb911faac290
ArtunALP <artunalp1@gmail.com>
2021-05-01 19:21:27 +0800

    Board size fully changable and interactions fixed

--- Program.cs
+++ Program.cs
@@ -5,16 +5,22 @@ namespace project1
 {
     class Program
     {
-           const int rows = 3; 
-           const int columns = 3;
+           static int rows; 
+           static int columns;
+          
         static void Main(string[] args)
         {
            Console.ForegroundColor = ConsoleColor.White;
            Console.WindowHeight = 40;
-           
-/*            const int rows = 3;
-           const int columns = 3;
- */        int turnCount = 0;
+
+           Console.WriteLine("input a number which will be the dimensions of your board.");
+          // int dimensions = Int32.Parse(Console.ReadLine());
+           rows = Int32.Parse(Console.ReadLine());
+           columns = Int32.Parse(Console.ReadLine());
+           //columns = Int32.Parse(Console.ReadLine());
+           Console.Clear();
+
+           int turnCount = 0;
            bool gameRunning = true;
            int player = 1;
 
@@ -32,39 +38,42 @@ namespace project1
            {
                int selection = Convert.ToInt32(Console.ReadLine());
 
-               int rowSelection = (selection - 1) / rows;
+               int rowSelection = (selection - 1) / columns;
                int columnSelection = (selection - 1) % columns;
-                 
-                if(player == 1)
-                {
-                    if(board[rowSelection,columnSelection] != 'X' && board[rowSelection,columnSelection] != 'O')
-                    {
-                    board[rowSelection,columnSelection] = 'X';
-                    turnCount++;
-                    player = PlayerSwitch(player);
-                    }
-                    else
-                    {
-                        player = 1;
-                    }
-                }
-                else if(player == 2)
-                {
-                    if(board[rowSelection,columnSelection] != 'X' && board[rowSelection,columnSelection] != 'O')
-                    {
-                    board[rowSelection,columnSelection] = 'O';
-                    player = PlayerSwitch(player);
-                    turnCount++;
-                    }
-                    else
-                    {
-                        player = 2;
-                    }
-                }
+
+             if(selection <= rows * columns &&  selection > 0)
+               {
+                   if(player == 1)
+                   {
+                        if(board[rowSelection,columnSelection] != 'X' && board[rowSelection,columnSelection] != 'O')
+                        {
+                            board[rowSelection,columnSelection] = 'X';
+                            turnCount++;
+                            player = PlayerSwitch(player);
+                        }
+                        else
+                        {
+                          player = 1;
+                        }
+                   }
+                   else if(player == 2)
+                   {
+                        if(board[rowSelection,columnSelection] != 'X' && board[rowSelection,columnSelection] != 'O')
+                        {
+                            board[rowSelection,columnSelection] = 'O';
+                            player = PlayerSwitch(player);
+                            turnCount++;
+                        }
+                        else
+                        {
+                           player = 2;
+                        }
+                   }
+                }   
 
                     Console.Clear();
                     PrintBoard(board);
-                //win conditions for player 1 
+                 //win conditions for player 1 
                 if(board[0,0] == 'X' && board[0,1] == board[0,0] && board[0,2] == board[0,0])
                 {
                     gameWinner = 1;
@@ -149,12 +158,26 @@ namespace project1
                 }
                 
                 //in the case of a draw
-                if(turnCount >= 9){
+/*                 if(turnCount >= 9){
                     gameWinner = 3;
                     gameRunning = false;
-                }
+                }  */
+                
+/*                 for (int i = 0; i < rows; i++)
+                {
+                    int[] colCheck = new int[rows];
 
+                    for(int j = 0; j < columns; j++)
+                    {
+                        colCheck[j] = j;
+                    }
 
+                    if(board[i,j] = 'X')
+                    {
+                        gameWinner = 1;
+                        gameRunning = false;
+                    }
+                } */
            }
             
             
@@ -185,6 +208,7 @@ namespace project1
 
         static void PrintBoard(char[,] board)
         {
+            //lines at the top of the board
             for (int lines = 0; lines < columns; lines++)
             {
                 Console.Write("----");
@@ -203,8 +227,9 @@ namespace project1
                   Console.Write(" | ");
               }   
               Console.WriteLine();
-           
-              for (int lines2 = 0; lines2 < rows; lines2++)
+
+              //Lines at the bottom of the '|'
+              for (int lines2 = 0; lines2 < columns; lines2++)
               {
                   Console.Write("----");
               }
